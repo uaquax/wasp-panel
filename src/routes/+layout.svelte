f5865af4e88a187450c77ec1aaa8e9bb9e4a2066
uaquax <uaquax@tutanota.com>
2023-12-13 23:52:35 +0800

    v0.1.0

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+  import "../app.css";
+</script>
+
+<section>
+  <header>
+    <div class="logo">WASP</div>
+  </header>
+  <slot />
+</section>
+
+<style>
+  header {
+    text-align: center;
+    background: #1f2335;
+    padding: 5px;
+  }
+
+  .logo {
+    color: #7aa2f7;
+  }
+</style>
